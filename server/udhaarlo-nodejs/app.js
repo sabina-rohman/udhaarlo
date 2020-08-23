e383b053d095de6439eb0b6218f21b5ccab4ffb0
Sabina Barbhuiya <sabina.barbhuiya@yahoo.com>
2020-08-23 22:59:56 +0100

    Added database connection and environment file

--- server/udhaarlo-nodejs/app.js
+++ server/udhaarlo-nodejs/app.js
@@ -1,13 +1,20 @@
+require('dotenv').config();
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// database setup
+mongoose.connect(process.env.DB_URL, 
+ { useNewUrlParser: true, useUnifiedTopology: true});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
